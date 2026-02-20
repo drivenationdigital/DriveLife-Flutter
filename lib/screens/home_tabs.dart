7ea787782662315d34bdbe67e217a394a92c05ce
Keshan <keshanth64@gmail.com>
2026-02-20 14:03:16 +0000

    Updated version + added refresh logic

--- lib/screens/home_tabs.dart
+++ lib/screens/home_tabs.dart
@@ -132,7 +132,7 @@ class _HomeTabsState extends State<HomeTabs> {
       print(
         '✅ Already have ${existingEntities.length} entities for user ${user.id}',
       );
-      return;
+      // return;
     }
 
     print('🔄 Loading managed entities for user ${user.id}');
